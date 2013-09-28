e467ee42e5d28b40962cbc3479e5c9d305820f10
merin <talktomerin@gmail.com>
2013-09-28 00:00:57 -0400

    testing the DB class

--- Expense/src/com/merin/expense/NewUserActivity.java
+++ Expense/src/com/merin/expense/NewUserActivity.java
@@ -14,19 +14,22 @@ public class NewUserActivity extends Activity {
 	private SQLiteDatabase db;
 	  private String[] allColumns = { MyDBHelper.COL_USERID,MyDBHelper.COL_PASSWORD,MyDBHelper.COL_EMAIL };
 	
-	
+	/*
 	public NewUserActivity(Context context) {
 		dbHelper = new MyDBHelper(context);
 
 
 	}
-
+*/
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_user);
+		dbHelper = new MyDBHelper(this);
+		sample1();
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -42,7 +45,14 @@ public class NewUserActivity extends Activity {
 		cv.put(MyDBHelper.COL_EMAIL, "talktomerin@gmail.com");
 		db.insert(MyDBHelper.TABLE_USR, null, cv);
 		
-		Cursor cursor = db.query(MyDBHelper.TABLE_USR,allColumns, MyDBHelper.COL_USERID + " = merin",null,null,null,null);
+		//Cursor cursor = db.query(MyDBHelper.TABLE_USR,allColumns, MyDBHelper.COL_USERID + " = merin",null,null,null,null);
+		Cursor cursor = db.query(true, MyDBHelper.TABLE_USR, allColumns, null, null, null, null, null, null, null);
+		if(cursor != null){
+			cursor.moveToFirst();
+			System.out.println(cursor.getString(0));
+			System.out.println(cursor.getString(1));
+			System.out.println(cursor.getString(2));
+		}
 	}
 
 }
